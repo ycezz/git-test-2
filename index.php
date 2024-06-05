d96dd2e2f5982b7ce7298f4d9c8b38170dff7518
ycezz <putrimiila.fx@gmail.com>
2024-06-05 10:39:16 +0800

    menambahkan paragraf

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coba Git Remote</title>
+</head>
+<body>
+    <h1>Learning Git remote</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum aliquam rerum culpa molestiae vitae autem, mollitia est laboriosam debitis, tenetur laborum enim modi, dolores iure nisi voluptatibus. Explicabo, nostrum quos.</p>
+</body>
+</html>
